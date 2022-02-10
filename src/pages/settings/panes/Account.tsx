094c34cfe3b254a2d06861b23a1e3e6d9f07b381
Chatnet.me <70344872+DisListMe@users.noreply.github.com>
2022-02-10 18:39:43 -0500

    Update Account.tsx

--- src/pages/settings/panes/Account.tsx
+++ src/pages/settings/panes/Account.tsx
@@ -215,7 +215,7 @@ export const Account = observer(() => {
                 <CategoryButton
                     icon={<Trash size={24} color="var(--error)" />}
                     description={
-                        "Delete your account, including all of your data. (sends an email to contact@chatnet.me)"
+                        "Delete your account, including all of your data. (sends an email to ops@chatnet.me)"
                     }
                     hover
                     action="external">
